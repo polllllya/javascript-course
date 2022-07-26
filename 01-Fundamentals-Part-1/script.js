8ba6f5debcac79bb74517ab8a7c228dec899b2a1
Palina Brahina <109469129+polllllya@users.noreply.github.com>
2022-07-26 16:38:16 +0200

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -82,3 +82,46 @@ console.log('Marks BMI is ' + bmiMark);
 console.log('Johns BMI is ' + bmiJohn);
 
 console.log('Mark has a higher BMI than John: ' + markHigherBMI);*/
+
+
+
+//STRING AND TEMPLATE LITERALS
+/*const country = 'Poland';
+const continent = 'Europe';
+const population = 37.95;
+
+const description = `${country} is in ${continent} and it's ${population} million people speak polish.`;
+
+console.log(description);*/
+
+
+
+//IF ELSE STATEMENTS
+/*const average = 33;
+const population = 37;
+
+if (population > average) {
+    console.log(`Poland's population is above average`);
+} else {
+    console.log(`Poland's population is ${average - population} million below average`);
+}*/
+
+
+
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+// Your tasks:
+// 1. Print a nice output to the console,saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2.Use a template literal to include the BMI values in the outputs. Example:"Mark's BMI (28.3) is higher than John's (23.9)!"
+
+/*const heightMark = 1.69, heightJohn = 1.95;
+const massMark = 78, massJohn = 92;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI(${bmiMark}) is higher than John's(${bmiJohn})!`)
+} else {
+    console.log(`John's BMI(${bmiJohn}) is higher than Mark's(${bmiMark})!`);
+}*/
