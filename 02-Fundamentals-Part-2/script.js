0d666b78356023ce9027156d463105e8f08e8609
Palina Brahina <109469129+polllllya@users.noreply.github.com>
2022-08-25 13:05:48 +0200

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -28,7 +28,45 @@ const percentageOfWorld2 = function (population) {
 console.log(`FUNCTION EXPRESSION: ${country} has ${population} million people, so it's ${percentageOfWorld2(population)}% of the world population.`);
 
 
-
 //ARROW FUNCTION
 const percentageOfWorld3 = populations => population * 100 / 7900;
 console.log(`FUNCTION ARROW: ${country} has ${population} million people, so it's ${percentageOfWorld3(population)}% of the world population.`);
+
+
+//FUNCTIONS CALLING OTHER FUNCTIONS
+const describePopulation = (country, population) => `${country} has ${population} million people, which is about ${percentageOfWorld1(population)} of the world.`;
+console.log(describePopulation(country, population));
+
+/*CODING CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to  calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1
+
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49 */
+
+const calcAverage = (one, two, three) => (one + two + three) / 3;
+
+const avgDolhins = calcAverage(85, 54, 41);
+const avgKoalas = calcAverage(23, 34, 27);
+
+const checkWinner = (avgDolhins, avgKoalas) => {
+    if (avgDolhins > avgKoalas * 2) {
+        return `Dolhins win (${avgDolhins} vs. ${avgKoalas})`;
+    } else if (avgKoalas > avgDolhins * 2) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolhins})`;
+    } else {
+        return 'A drow won';
+    }
+}
+
+console.log(checkWinner(avgDolhins, avgKoalas));
+
+
