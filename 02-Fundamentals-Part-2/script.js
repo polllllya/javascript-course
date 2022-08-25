bffd81a2d467bb22a624f633ce744d2c92d041e4
Palina Brahina <109469129+polllllya@users.noreply.github.com>
2022-08-25 21:50:37 +0200

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -100,3 +100,27 @@ if (neighbours.includes('Germany')) {
 
 neighbours[neighbours.indexOf('Czech')] = 'Czech Republic';
 console.log(neighbours);
+
+
+
+/*CODING CHALLENGE #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill valueas an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data below
+3. Create an array 'tips' containg the tip value for each bill,calculated from the function you created before
+
+Test data: 125, 555 and 44 */
+
+const calcTip = function (billValueas) {
+    if (billValueas > 50 && billValueas < 300) {
+        return 15 * billValueas / 100;
+    } else {
+        return 20 * billValueas / 100;
+    }
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(tips);
